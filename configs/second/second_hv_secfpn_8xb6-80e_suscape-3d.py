61aebc6f619baf3b7eb6e9cd255b5a8ae3c6d219
Naurril <naurril@gmail.com>
2023-06-01 01:50:09 +0000

    second

--- configs/second/second_hv_secfpn_8xb6-80e_suscape-3d.py
+++ configs/second/second_hv_secfpn_8xb6-80e_suscape-3d.py
@@ -1,5 +1,5 @@
 _base_ = [
     '../_base_/models/second_hv_secfpn_suscape.py',
-    '../_base_/datasets/suscape_3d.py',
+    '../_base_/datasets/suscape-3d.py',
     '../_base_/schedules/cyclic-40e.py', '../_base_/default_runtime.py'
 ]
