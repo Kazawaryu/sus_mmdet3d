f3350a6982ace8c9db750030d086d4745f031a2e
Kazawaryu <12011126@mail.sustech.edu.cn>
2024-04-28 07:36:55 +0000

    point rcnn

--- configs/point_rcnn/point_rcnn_2x8_suscape-new.py
+++ configs/point_rcnn/point_rcnn_2x8_suscape-new.py
@@ -92,45 +92,56 @@ val_dataloader = dict(
 )
 
 lr = 0.0001
-epoch_num = 40
-optim_wrapper = dict(optimizer=dict(lr=lr), clip_grad=dict(max_norm=35, norm_type=2))
+optim_wrapper = dict(optimizer=dict(lr=lr, betas=(0.95, 0.85)))
+num_epochs = 40
+train_cfg = dict(by_epoch=True, max_epochs=num_epochs, val_interval=1)
+
+auto_scale_lr = dict(enable=False, base_batch_size=32)
 param_scheduler = [
+    # learning rate scheduler
+    # During the first 35 epochs, learning rate increases from 0 to lr * 10
+    # during the next 45 epochs, learning rate decreases from lr * 10 to
+    # lr * 1e-4
     dict(
         type="CosineAnnealingLR",
-        T_max=epoch_num * 0.4,
+        T_max=num_epochs * 0.4,
         eta_min=lr * 10,
         begin=0,
-        end=epoch_num * 0.4,
+        end=num_epochs * 0.4,
         by_epoch=True,
         convert_to_iter_based=True,
     ),
     dict(
         type="CosineAnnealingLR",
-        T_max=epoch_num * 0.6,
+        T_max=num_epochs * 0.6,
         eta_min=lr * 1e-4,
-        begin=epoch_num * 0.4,
-        end=epoch_num * 1,
+        begin=num_epochs * 0.4,
+        end=num_epochs,
         by_epoch=True,
         convert_to_iter_based=True,
     ),
+    # momentum scheduler
+    # During the first 35 epochs, momentum increases from 0 to 0.85 / 0.95
+    # during the next 45 epochs, momentum increases from 0.85 / 0.95 to 1
     dict(
         type="CosineAnnealingMomentum",
-        T_max=epoch_num * 0.4,
+        T_max=num_epochs * 0.4,
         eta_min=0.85 / 0.95,
         begin=0,
-        end=epoch_num * 0.4,
+        end=num_epochs * 0.4,
         by_epoch=True,
         convert_to_iter_based=True,
     ),
     dict(
         type="CosineAnnealingMomentum",
-        T_max=epoch_num * 0.6,
+        T_max=num_epochs * 0.6,
         eta_min=1,
-        begin=epoch_num * 0.4,
-        end=epoch_num * 1,
+        begin=num_epochs * 0.4,
+        end=num_epochs,
+        by_epoch=True,
         convert_to_iter_based=True,
     ),
 ]
 
-train_cfg = dict(by_epoch=True, max_epochs=epoch_num, val_interval=20)
-val_cfg = dict()
+# train_cfg = dict(by_epoch=True, max_epochs=epoch_num, val_interval=20)
+# val_cfg = dict()
